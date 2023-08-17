e9c1ec0b60e44eff90c65992fd1acde91d00e4c3
Ozay57 <oaydin703@gmail.com>
2023-08-17 15:09:29 +0300

    .

---
+++ src/veritanim.java
@@ -0,0 +1,18 @@
+
+public class veritanim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int sayi = 125;
+      System.out.println(sayi);
+      
+      //eclipse code complete: windows/preferences/java/editor/content assist
+      //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._      
+      
+      long a = Long.MAX_VALUE;
+      System.out.println(a);
+      
+      //byte/short/int/long/float/double/boolean/char/string
+	}
+
+}
